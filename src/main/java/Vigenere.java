b5e69a0eb9568960fdb1ab124898259b46210b2e
Jeremy-Lu-APComp <21jlu@athenian.org>
2020-10-17 16:33:04 +0000

    Added code for vigenere

--- src/main/java/Vigenere.java
+++ src/main/java/Vigenere.java
@@ -1,52 +1,69 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 //work in progress - NOT SURE HOW TO PROCEED
 public class Vigenere {
-    // char msg[] = //{characters of message}
-	// char key[] = //{characters of key}
-    // int msg_Length = msg.length;
-	// char newKey[] = char[msg_Length];
-	// char encrypted_message[] = char[msg_Length];
-	// char decrypted_message[] = char[msg_Length];
-		
-    // public static String encryptVigenere(String message, String key) {
-    // for(i = 0; i < msg_Length; ++i){
-	// 	encrypted_message[i] = (char)(((msg[i] + newKey[i]) % 26) + 'A');
-    // }
-    // }
 
-    // public static String decryptVigenere(String message, String key) {
-    //     for(i = 0; i < msg_Length; ++i){
-	// 		decrypted_message[i] = (char)((((decrypted_message[i] - newKey[i]) + 26) % 26) + 'A');
-    //     }
-    // }
+    public static String decryptVigenere(String message, String key) {
+        String encrypt_message = message;
+        String new_string = "";
+        int key_index = 0;
+        for(int counter = 0; counter<encrypt_message.length(); counter++){
+            char n = encrypt_message.charAt(counter);
+            if (n >= 'A' && n <= 'z'){
+                n = (char) (n - key.charAt(key_index));
+                key_index++;
+                if(key_index >= key.length()){
+                    key_index = 0;
+                }
+                new_string += n;
+            }
+        }
+        return new_string;
+    }
+    public static String encryptVigenere(String message, String key) {
+        String encrypt_message = message;
+        String new_string = "";
+        int key_index = 0;
+        for(int counter = 0; counter<encrypt_message.length(); counter++){
+            char n = encrypt_message.charAt(counter);
+            if (n >= 'A' && n <= 'z'){
+                n = (char) (n + key.charAt(key_index));
+                key_index++;
+                if(key_index >= key.length()){
+                    key_index = 0;
+                }
+                new_string += n;
+            }
+        }
+        return new_string;
+    }
 
 
-    // public static void main(String[] args) {
-    //     Scanner scan = new Scanner(System.in);
+     public static void main(String[] args) {
+         Scanner scan = new Scanner(System.in);
 
-    //     System.out.println("Would you like to \"encrypt\" or \"decrypt\" a message?");
-    //     String command = scan.nextLine().trim().toLowerCase();
+        System.out.println("Would you like to \"encrypt\" or \"decrypt\" a message?");
+         String command = scan.nextLine().trim().toLowerCase();
 
-    //     if (command.equals("encrypt")) {
-    //         System.out.println("Please enter your message to be encrypted: ");
-    //         String message = scan.nextLine();
-    //         System.out.println("Please enter the key for your message: ");
-    //         String key = scan.nextLine().trim().toUpperCase();
-    //         System.out.println("Here is your encrypted message: ");
-    //         System.out.println(encryptVigenere(message, key));
-    //     }
-    //     else if (command.equals("decrypt")) {
-    //         System.out.println("Please enter your message to be decrypted: ");
-    //         String message = scan.nextLine();
-    //         System.out.println("Please enter the key for your message: ");
-    //         String key = scan.nextLine().trim().toUpperCase();
-    //         System.out.println("Here is your decrypted message: ");
-    //         System.out.println(decryptVigenere(message, key));
-    //     }
-    //     else {
-    //         System.out.println("Unknown command; please type either \"encrypt\" or \"decrypt\"");
-    //     }
+         if (command.equals("encrypt")) {
+             System.out.println("Please enter your message to be encrypted: ");
+             String message = scan.nextLine();
+             System.out.println("Please enter the key for your message: ");
+             String key = scan.nextLine().trim().toUpperCase();
+             System.out.println("Here is your encrypted message: ");
+             System.out.println(encryptVigenere(message, key));
+         }
+         else if (command.equals("decrypt")) {
+             System.out.println("Please enter your message to be decrypted: ");
+             String message = scan.nextLine();
+             System.out.println("Please enter the key for your message: ");
+             String key = scan.nextLine().trim().toUpperCase();
+             System.out.println("Here is your decrypted message: ");
+             System.out.println(decryptVigenere(message, key));
+         }
+        else {
+             System.out.println("Unknown command; please type either \"encrypt\" or \"decrypt\"");
+         }
 
-    //     scan.close();
-    // }
+        scan.close();
+    }
 }
